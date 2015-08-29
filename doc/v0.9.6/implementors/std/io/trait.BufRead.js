30cc4e0fe4aa262c51d4ef8374ee58b547ea5f72
Steven Fackler <sfackler@gmail.com>
2015-08-28 17:04:33 -0700

    Release v0.9.6

---
+++ doc/v0.9.6/implementors/std/io/trait.BufRead.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.BufRead.html' title='std::io::BufRead'>BufRead</a> for <a class='struct' href='postgres/stmt/struct.CopyOutReader.html' title='postgres::stmt::CopyOutReader'>CopyOutReader</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/v0.9.6/implementors/std/io/trait.Read.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='postgres/io/struct.Stream.html' title='postgres::io::Stream'>Stream</a>","impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Read.html' title='std::io::Read'>Read</a> for <a class='struct' href='postgres/stmt/struct.CopyOutReader.html' title='postgres::stmt::CopyOutReader'>CopyOutReader</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
