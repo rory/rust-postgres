0b242a7dec9b857f81480cb24f1905351abc156a
Steven Fackler <sfackler@gmail.com>
2015-11-07 16:45:53 -0800

    Release v0.10.1

---
+++ doc/v0.10.1/implementors/core/iter/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['postgres'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='postgres/rows/struct.Rows.html' title='postgres::rows::Rows'>Rows</a>&lt;'a&gt;","impl&lt;'stmt&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for <a class='struct' href='postgres/rows/struct.Rows.html' title='postgres::rows::Rows'>Rows</a>&lt;'stmt&gt;","impl&lt;'a, 'conn&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.IntoIterator.html' title='core::iter::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='postgres/notification/struct.Notifications.html' title='postgres::notification::Notifications'>Notifications</a>&lt;'conn&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
